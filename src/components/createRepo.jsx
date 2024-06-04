3e770cd6714c92e68a78bd3cec6f5b04ba57a5c7
Babatunde <tundeloper@gmail.com>
2024-06-04 15:16:16 +0100

    create repos

--- src/components/createRepo.jsx
+++ src/components/createRepo.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react"
 
 function CreateRepo ({toggle}) {
-    // const token = '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'
+    const token = ''
 
     const config = {
         headers: {
@@ -31,7 +31,7 @@ function CreateRepo ({toggle}) {
             private: isPrivate
         }
         try {
-            const response = await axios.get(apiUrl, data, config)
+            const response = await axios.post(apiUrl, data, config)
             console.log(response.data, 'data')
         } catch (error) {
             console.log(error)
